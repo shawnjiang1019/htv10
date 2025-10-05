d8c6f98f48b7f046d8ba3495b6d85372c4ee65dd
Nethanel Reta <nethanel.n4@gmail.com>
2025-10-04 22:55:32 -0400

    .

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 from video_transcription.transcribe import router as vid_router
 from video_transcription.article_transcribe import router as article_router
 from agents_debate.debate_router import router as debate_router
-from agents_debate.data_router import router as data_router
+# from agents_debate.data_router import router as data_router
 
 app = FastAPI()
 
 app.include_router(vid_router, prefix="/vid")
 app.include_router(article_router, prefix="/article")
 app.include_router(debate_router, prefix="/debate")
-app.include_router(data_router, prefix="/data")
+# app.include_router(data_router, prefix="/data")
 
 
